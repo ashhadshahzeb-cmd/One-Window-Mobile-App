8c489f2de6afa3cfdbed64c47897df9bfc9f7df9
ashhad90 <ashhadshahzeb051@gmail.com>
2026-05-06 10:29:28 +0500

    feat: setup capacitor and github actions for automatic APK build

---
+++ android/app/src/main/java/com/kwsb/tracking/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kwsb.tracking;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kwsb.tracking',
+  appName: 'KWSC Tracking',
+  webDir: 'dist'
+};
+
+export default config;
